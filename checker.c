06b9260b09d7031ddc9cf41444baebbd6d965a78
Vijay K S <57295466+vijay1421@users.noreply.github.com>
2024-08-26 10:40:11 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <assert.h>
 
-//A function to check the temperature of battery
-int batteryTemp(float Temp){
-  if(Temp < 0 || Temp > 45) {
+int batteryTemp(float temperature) {
+  if (temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
-    return 1;
-  }
-  else{
+    return 1; // Error
+  } else {
     printf("Temperature is OK\n");
-    return 0;
+    return 0; // OK
   }
 }
-
 int checkSOC(float soc) {
-   if(soc < 20 || soc > 80) {
+   if (soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
-    return 1;
+    return 1; // Error
+   } else {
+    printf("State of Charge is OK\n");
+    return 0; // OK
    }
-    else{
-      printf("State of Charge is OK\n");
-      return 0;
-    }
 }
 
-int chargeRateCheck(float chargeRate){
-  if(chargeRate > 0.8) {
+int chargeRateCheck(float chargeRate) {
+  if (chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
-    return 1;
-  }
-  else{
+    return 1; // Error
+  } else {
     printf("Charge Rate is OK\n");
-    return 0;
+    return 0; // OK
   }
 }
 
-
+// Simplified main function to check all battery conditions
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(batteryTemp(temperature)) return 1;
-  if(checkSOC(soc)) return 1;
-  if(chargeRateCheck(chargeRate)) return 1;
-  return 0;
+  if (batteryTemp(temperature)) return 1;
+  if (checkSOC(soc)) return 1;
+  if (chargeRateCheck(chargeRate)) return 1; 
+  return 0; 
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(batteryIsOk(50, 85, 0));
+  // Test cases
+  assert(batteryIsOk(25, 70, 0.7)); 
+  assert(batteryIsOk(50, 85, 0)); 
+  return 0;
+}
